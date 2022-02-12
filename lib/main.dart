cbf7a2be6ab5a26aecb7914693569dfe38476ccd
Shripal Jain <shripal17@gmail.com>
2022-02-12 18:33:29 +0530

    Hide debug mode banner
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MoEngage Articles',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
